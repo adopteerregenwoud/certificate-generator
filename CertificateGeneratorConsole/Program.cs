d9b80b56e66cfe1d052856099b2a808e1a8ce59d
Sebastiaan Megens <sebastiaan.megens@greenchoice.nl>
2024-06-20 11:56:37 +0200

    Changed: creating a directory per adoption so the filenames will be simple.

--- CertificateGeneratorConsole/Program.cs
+++ CertificateGeneratorConsole/Program.cs
@@ -31,24 +31,26 @@ internal class Program
     {
         CertificateGenerator.Result result = certificateGenerator.Generate(adoptionRecord);
 
-        string fileBasename = GenerateFileBasename(adoptionRecord);
+        string fileBasename = GenerateBasename(adoptionRecord);
+        string dir = Path.Combine(outputDirectory, fileBasename);
+        Directory.CreateDirectory(dir);
 
-        string outputPath = Path.Combine(outputDirectory, $"{fileBasename}.png");
+        string outputPath = Path.Combine(dir, "certificaat.png");
         Console.WriteLine($"    Writing full-size PNG to {outputPath}...");
         using var outputStream = File.Create(outputPath);
         result.FullSizePngStream.Seek(0, SeekOrigin.Begin);
         result.FullSizePngStream.CopyTo(outputStream);
 
-        outputPath = Path.Combine(outputDirectory, $"{fileBasename}.jpg");
+        outputPath = Path.Combine(dir, "certificaat.jpg");
         Console.WriteLine($"    Writing 3MB JPG to {outputPath}...");
         using var outputStreamJpg = File.Create(outputPath);
         result.Jpg3MbStream.Seek(0, SeekOrigin.Begin);
         result.Jpg3MbStream.CopyTo(outputStreamJpg);
     }
 
-    private static string GenerateFileBasename(AdoptionRecord adoptionRecord)
+    private static string GenerateBasename(AdoptionRecord adoptionRecord)
     {
-        return $"certificate-{adoptionRecord.Name}-{adoptionRecord.Date:yyyy-MM-dd}";
+        return $"{adoptionRecord.Date:yyyy-MM-dd}-{adoptionRecord.Name}";
     }
 
     private static IEnumerable<AdoptionRecord> ParseExcel(string excelPath)
