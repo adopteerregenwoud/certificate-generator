a7dfcb6fbc6b7a072a52576ed25860e07f68e231
Sebastiaan Megens <sebastiaan.megens@greenchoice.nl>
2024-06-20 06:48:17 +0200

    Added: long name to test excel file.

--- CertificateGeneratorCoreTests/ExcelAdoptionRecordGeneratorTests.cs
+++ CertificateGeneratorCoreTests/ExcelAdoptionRecordGeneratorTests.cs
@@ -17,7 +17,7 @@ public class ExcelAdoptionRecordsParserTests
         var expectedRecords = new List<AdoptionRecord>
         {
             new ("John Doe", 150,   new DateOnly(2023, 06, 01), Language.English),
-            new ("Jane Smith", 200, new DateOnly(2023, 06, 02), Language.Dutch),
+            new ("Jane Smith en ook nog vele anderen", 200, new DateOnly(2023, 06, 02), Language.Dutch),
             new ("Max Mustermann", 180, new DateOnly(2023, 06, 03), Language.English),
             new ("María García", 220, new DateOnly(2023, 06, 04), Language.Dutch),
             new ("François Dupont", 170, new DateOnly(2023, 06, 05), Language.Dutch)
