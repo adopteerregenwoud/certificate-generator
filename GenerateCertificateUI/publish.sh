5c29c1319b5f23810d7d2c7413cac31f6b415db2
Sebastiaan Megens <sebastiaan@blauwe-lucht.nl>
2024-07-06 06:20:19 +0200

    Added: timestamp to application window.

--- GenerateCertificateUI/publish.sh
+++ GenerateCertificateUI/publish.sh
@@ -8,27 +8,14 @@ set -euo pipefail
 APP_NAME=GenerateCertificateUI
 DIST_DIR=./dist
 
-rawurlencode() {
-  local string="${1}"
-  local strlen=${#string}
-  local encoded=""
-  local pos c o
-
-  for (( pos=0 ; pos<strlen ; pos++ )); do
-     c=${string:$pos:1}
-     case "$c" in
-        [-_.~a-zA-Z0-9] ) o="${c}" ;;
-        * )               printf -v o '%%%02x' "'$c"
-     esac
-     encoded+="${o}"
-  done
-  echo "${encoded}"    # You can either set a return variable (FASTER) 
-  REPLY="${encoded}"   #+or echo the result (EASIER)... or both... :p
-}
-
 if [ -d $DIST_DIR ]; then
     rm -rf $DIST_DIR
 fi
+
+TIMESTAMP=`date +'%Y-%m-%d_%H%M'`
+
+echo "Adding timestamp to application..."
+sed -i "s/__version_timestamp__/$TIMESTAMP/" MainWindow.axaml
 
 echo "Running unit tests for $APP_NAME..."
 dotnet test ../CertificateGeneratorCoreTests
@@ -39,11 +26,13 @@ dotnet publish -c Release -r win-x64 \
    -p:IncludeAllContentForSelfExtract=true -p:TrimUnusedDependencies=true \
    --output $DIST_DIR
 
+echo "Removing timestamp from source code..."
+sed -i "s/$TIMESTAMP/__version_timestamp__/" MainWindow.axaml
+
 echo "Deleting old zip files..."
 rm -f /tmp/$APP_NAME*.zip
 
 echo "Creating zip file..."
-TIMESTAMP=`date +'%Y-%m-%d_%H%M'`
 ZIP_NAME=${APP_NAME}-$TIMESTAMP.zip
 ZIP_PATH=/tmp/$ZIP_NAME
 cd $DIST_DIR && zip -r $ZIP_PATH *
